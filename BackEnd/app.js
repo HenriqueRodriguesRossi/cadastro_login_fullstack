b512cd5f04e9200e8f04be4836dbab8407a4b4c9
HenriqueRodriguesRossi <hrodriguescruciakrossi46@gmail.com>
2023-08-17 18:41:55 -0300

    Pequena alteracao no backend, adicionando a biblioteca cors

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 require("dotenv").config()
 require("./db/connection")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 const UsuarioRouter = require("./router/UsuarioRouter")
 app.use(UsuarioRouter)
