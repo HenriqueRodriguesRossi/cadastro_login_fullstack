2e2cb7fbaa62df5c985d1976d47bb308e20bf6ae
HenriqueRodriguesRossi <hrodriguescruciakrossi46@gmail.com>
2023-08-21 17:10:44 -0300

    Adicionando validacao de campos com o Yup

---
+++ FrontEnd/cad_admin/src/utils/captureErroYup.js
@@ -0,0 +1,6 @@
+export const captureErroYup = (error) =>
+    error.inner.reduce((acc, currentValue) => {
+        const data = { ...acc }
+        data[currentValue.path] = currentValue.message
+        return data
+    }, {})
--- FrontEnd/cad_admin/src/views/Cadastro/Cadastro.vue
+++ FrontEnd/cad_admin/src/views/Cadastro/Cadastro.vue
@@ -4,28 +4,32 @@
         <form id="form" class="form" @submit.prevent="handlerCadastro">
             <div class="campos">
                 <label for="nome">Nome:</label> <br>
-                <input type="text" name="nome" id="nome" placeholder="Digite o seu nome" class="inputs" v-model="nome">
+                <input type="text" name="nome" id="nome" placeholder="Digite o seu nome" class="inputs" v-model="nome"> <br>
+                <span>{{ inputError }}</span>
             </div>
             <div class="campos">
                 <label for="sobrenome">Sobrenome:</label> <br>
                 <input type="text" name="sobrenome" id="sobrenome" placeholder="Digite o seu sobrenome" class="inputs"
-                    v-model="sobrenome">
+                    v-model="sobrenome"> <br>
+                <span>{{ inputError }}</span>
             </div>
             <div class="campos">
                 <label for="nomeDeUsuario">Nome de Usuário:</label> <br>
                 <input type="text" name="nomeDeUsuario" id="nomeDeUsuario" placeholder="Digite o seu nome de usuário"
-                    class="inputs" v-model="nomeDeUsuario">
+                    class="inputs" v-model="nomeDeUsuario"> <br>
+                <span>{{ inputError }}</span>
             </div>
             <div class="campos">
                 <label for="senha">Senha:</label> <br>
                 <input type="password" name="senha" id="senha" placeholder="Digite a sua senha" class="inputs"
-                    v-model="senha">
+                    v-model="senha"> <br>
                     <span>{{ inputError }}</span>
             </div>
             <div class="campos">
                 <label for="repitaSenha">Repita a Senha:</label> <br>
                 <input type="password" name="repitaSenha" id="repitaSenha" placeholder="Digite a sua senha novamente"
-                    class="inputs" v-model="repitaSenha">
+                    class="inputs" v-model="repitaSenha"> <br>
+                <span>{{ inputError }}</span>
             </div>
             <button type="submit">Cadastrar</button>
         </form>
@@ -33,8 +37,11 @@
 </template>
 
 <script>
+import * as Yup from "yup"
+import {captureErroYup} from "../../utils/captureErroYup"
 import axios from 'axios'
 const url = 'http://localhost:8080/nova/conta'
+
 export default {
     name: "Login",
     data() {
@@ -43,11 +50,31 @@ export default {
             sobrenome: "",
             nomeDeUsuario: "",
             senha: "",
-            repitaSenha: ""
+            repitaSenha: "",
+
+            inputError: ""
         }
     },
     methods: {
         handlerCadastro() {
+            const validationSchema = Yup.object().shape({
+                nome: Yup.string().required(this.inputError = "O nome é obrigatório!").min(3, this.inputError = "O nome deve ter no mínimo 3 caracteres!"),
+                sobrenome: Yup.string().required(this.inputError = "O sobrenome é obrigatório!").min(4, this.inputError = "O sobrenome deve ter no mínimo 4 caracteres!"),
+                nomeDeUsuario: Yup.string().required(this.inputError = "O nome de usuário é obrigatório!").min(5, this.inputError = "Nome de usuário deve ter no mínimo 5 caracteres!"),
+                senha: Yup.string().required( this.inputError = "Nome de usuário é obrigatório!").min(8,  this.inputError = "Senha deve ter no mínimo 8 caracteres!"),
+                repitaSenha: Yup.string().required( this.inputError = "O campo repita a senha é obrigatório!")
+            })
+
+            validationSchema.validateSync(
+          {
+            nome: this.nome,
+            sobrenome: this.sobrenome,
+            nomeDeUsuario: this.nomeDeUsuario,
+            senha: this.senha,
+            repitaSenha: this.repitaSenha
+          },
+          {abortEarly: false}
+        )
             axios.post(url, {
                 nome: this.nomeDeUsuario,
                 sobrenome: this.sobrenome,
@@ -55,12 +82,9 @@ export default {
                 senha: this.senha,
                 repitaSenha: this.repitaSenha
             }).then(function (response) {
-                console.log(response.request.responseText);
+                console.log("Usuário cadastrado com sucesso: " + response);
             }).catch(function (error) {
-                console.error(error)
-                let responseText = JSON.parse(error.request.responseText)
-                console.log(responseText.mensagem)
-                input
+                console.error("Erro ao cadastrar o usuário: " + error)
             });
         }
     }
@@ -108,6 +132,12 @@ label{
 
 .inputs:focus{
     border: 2.5px solid dodgerblue;
+}
+
+span{
+    font-size: 16px;
+    color: red;
+    padding: 10px;
 }
 
 button{
